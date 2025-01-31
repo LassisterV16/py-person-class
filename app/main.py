3f2a1d348eca20472cda4ba9c8a817b247dc6209
LassisterV16 <lassisterv16@gmail.com>
2025-01-31 17:49:00 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people_list: list) -> list:
+    person_list = [
+        Person(person.get("name"), person.get("age")) for person in people_list
+    ]
+    for i in range(len(people_list)):
+        if people_list[i].get("wife") or people_list[i].get("husband"):
+            for index in range(len(people_list)):
+                if people_list[i].get("wife") == person_list[index].name:
+                    person_list[i].wife = person_list[index]
+                    break
+                elif people_list[i].get("husband") == person_list[index].name:
+                    person_list[i].husband = person_list[index]
+                    break
+
+    return person_list
